ce6443767c2b6b46cd9024984710c084905c0e9e
Aline <aline_lima@outlook.com>
2024-02-02 16:20:28 -0300

    Fluxo 3: cria model do sequelize

---
+++ app/backend/src/database/models/matches/MatchesModel.ts
@@ -0,0 +1,10 @@
+// export default class MatchesModel implements IMatchesModel {
+//   private model = SequelizeMatches;
+
+//   async findAll(): Promise<IMatches[]> {
+//     const getAll = await this.model.findAll();
+//     return getAll.map(({ id, date, teamA, teamB, scoreA, scoreB }) => ({ id, date, teamA, teamB, scoreA, scoreB }));
+//   }
+
+//     }
+// }
